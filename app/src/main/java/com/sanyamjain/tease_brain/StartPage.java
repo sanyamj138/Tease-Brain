b1b788aedc2b21b8ca11c42350a970b1085a9b1a
sanyamj138 <sanyamj138@gmail.com>
2022-05-14 23:03:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/sanyamjain/tease_brain/StartPage.java
@@ -0,0 +1,14 @@
+package com.sanyamjain.tease_brain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_page);
+    }
+}
