edb2af26e5e15558c8bf9fc01ce50ddf7e70ab30
sanyamj138 <sanyamj138@gmail.com>
2022-05-15 16:58:01 +0530

    TicTacToe activity added

--- app/src/main/java/com/sanyamjain/tease_brain/HomePage.java
+++ app/src/main/java/com/sanyamjain/tease_brain/HomePage.java
@@ -2,13 +2,16 @@ package com.sanyamjain.tease_brain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class StartPage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_page);
+        setContentView(R.layout.home_page);
     }
+
+
 }
---
+++ app/src/main/java/com/sanyamjain/tease_brain/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.sanyamjain.tease_brain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicTacToe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tac_toe);
+    }
+}
