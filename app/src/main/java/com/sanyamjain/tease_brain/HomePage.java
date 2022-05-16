66382b52b8383a533a6b634602a0f584693fe339
sanyamj138 <sanyamj138@gmail.com>
2022-05-16 10:43:49 +0530

    TicTacToe Intent Added

--- app/src/main/java/com/sanyamjain/tease_brain/HomePage.java
+++ app/src/main/java/com/sanyamjain/tease_brain/HomePage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -11,7 +13,20 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-    }
 
+        Button ticTacToe, guessThatNumber, teaseBrain;
+
+        ticTacToe = findViewById(R.id.ticTacToe);
+        guessThatNumber = findViewById(R.id.guessThatNumber);
+        teaseBrain = findViewById(R.id.teaseBrain);
+
+        ticTacToe.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent ticTacToeIntent = new Intent(HomePage.this, TicTacToe.class);
+                startActivity(ticTacToeIntent);
+            }
+        });
+    }
 
 }
