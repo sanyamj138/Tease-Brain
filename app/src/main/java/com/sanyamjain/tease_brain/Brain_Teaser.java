a57680efd91d4af400aa31e5e8ced0adc4a9455f
sanyamj138 <sanyamj138@gmail.com>
2022-05-28 09:45:55 +0530

    Added Brain Teaser Activity

---
+++ app/src/main/java/com/sanyamjain/tease_brain/Brain_Teaser.java
@@ -0,0 +1,14 @@
+package com.sanyamjain.tease_brain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Brain_Teaser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brain_teaser);
+    }
+}
